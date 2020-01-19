577a7225b1f1ca3e6ffb9132c580e44fe5ae4fcb
Alex Maloney <maloney.a12@gmail.com>
2020-01-19 23:24:16 +0000

    Add logging to script

--- nlp_test.py
+++ nlp_test.py
@@ -1,10 +1,24 @@
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
+# eventually we should track actor too 
+sentences = [
+  "I read The Pale King",
+  "last week I took a trip to California",
+  "Yesterday I had coffee with Rachel at Herkimer and read Dune",
+  "Autonomous cars shift insurance liability toward manufacturers"
+]
 
-print("Enter Sentence: ")
-sentence = str(input())
+print("select input")
+for i,s in enumerate (sentences): 
+    print(i, s)
 
+sentence_idx = int(input())
+doc = nlp(sentences[sentence_idx])
+
+# PARSE TODO
+# Thing = Noun subject 
+# Event = verb root + dependent object
 def getEntityOrGuess(nlp_doc):
     ents = list(nlp_doc.ents)
     if len(ents) == 0:
@@ -20,13 +34,20 @@ def getVerb(nlp_doc):
             return token.text
 
 
-doc = nlp(sentence)
 print("Entity: ")
 print(getEntityOrGuess(doc))
 print("Verb: ")
 print(getVerb(doc))
 
-print("==================")
+
 print("DEBUG DUMP")
+print("=======Token Info=========")
+print("text", "pos", "tag", "dep", "children")
 for token in doc:
-     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
+     print(token.text, token.pos_, token.tag_, token.dep_, [child for child in token.children])
+
+
+print("=======Noun Chunks========")
+print("text", "root", "root dep", "root head text")
+for chunk in doc.noun_chunks:
+    print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
