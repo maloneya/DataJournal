fdcf3bcaee24319c0270fe4d7a4bbf782e981dd4
Alex Maloney <maloney.a12@gmail.com>
2020-01-20 10:53:45 -0800

    Set up test framework

--- nlp_test.py
+++ nlp_test.py
@@ -1,21 +1,6 @@
 import spacy
 nlp = spacy.load("en_core_web_sm")
 
-# eventually we should track actor too 
-sentences = [
-  "I read The Pale King",
-  "last week I took a trip to California",
-  "Yesterday I had coffee with Rachel at Herkimer and read Dune",
-  "Autonomous cars shift insurance liability toward manufacturers"
-]
-
-print("select input")
-for i,s in enumerate (sentences): 
-    print(i, s)
-
-sentence_idx = int(input())
-doc = nlp(sentences[sentence_idx])
-
 # PARSE TODO
 # Thing = Noun subject 
 # Event = verb root + dependent object
@@ -28,26 +13,45 @@ def getEntityOrGuess(nlp_doc):
     
     return ents[0].text
 
-def getVerb(nlp_doc):
+def getEvent(nlp_doc):
     for token in nlp_doc:
         if token.pos_ == "VERB":
             return token.text
 
-
-print("Entity: ")
-print(getEntityOrGuess(doc))
-print("Verb: ")
-print(getVerb(doc))
-
-
-print("DEBUG DUMP")
-print("=======Token Info=========")
-print("text", "pos", "tag", "dep", "children")
-for token in doc:
-     print(token.text, token.pos_, token.tag_, token.dep_, [child for child in token.children])
+def parseSentence(sentence, show_debug_output=False):
+    doc = nlp(sentence)
+    if show_debug_output:
+        showDebug(doc)
+    return [getEvent(doc), getEntityOrGuess(doc)]
 
 
-print("=======Noun Chunks========")
-print("text", "root", "root dep", "root head text")
-for chunk in doc.noun_chunks:
-    print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+def showDebug(doc):
+    print("DEBUG DUMP")
+    print("=======Token Info=========")
+    print("text", "pos", "tag", "dep", "children")
+    for token in doc:
+        print(token.text, token.pos_, token.tag_, token.dep_, [child for child in token.children])
+
+
+    print("=======Noun Chunks========")
+    print("text", "root", "root dep", "root head text")
+    for chunk in doc.noun_chunks:
+        print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+
+if __name__ == "__main__":
+    # eventually we should track actor too 
+    sentences = [
+    "I read The Pale King",
+    "last week I took a trip to California",
+    "Yesterday I had coffee with Rachel at Herkimer and read Dune",
+    "Autonomous cars shift insurance liability toward manufacturers"
+    ]
+
+    print("select input")
+    for i,s in enumerate (sentences): 
+        print(i, s)
+
+    sentence_idx = int(input())
+    event_entity = parseSentence(sentences[sentence_idx], True)
+    print("=====output=====")
+    print ("Entity, verb", event_entity)
---
+++ sentence_tester.py
@@ -0,0 +1,24 @@
+from nlp_test import parseSentence
+
+input_output = [
+  ["I read The Pale King","read" , "The Pale King"],
+  ["last week I took a trip to California", "took a trip", "California"],
+  ["Yesterday I had coffee with Rachel at Herkimer", "had coffee", "Herkimer"],
+  ["Autonomous cars shift insurance liability toward manufacturers", "shift insurance liability", "Autonomous cars"]
+]
+
+def runTest(test_case):
+    sentence, event, entity = [val for val in test_case]
+    print("Case: ", sentence)
+    print("Expected", event, entity)
+    actual_event_entity = parseSentence(sentence)
+    if actual_event_entity[0] != event:
+        print("FAIL: event doenst match, got", actual_event_entity[0])
+        return
+    if actual_event_entity[1] != entity:
+        print("FAIL: entity doenst match, got", actual_event_entity[1])
+        return
+    print("PASSED")
+
+for test_case in input_output:
+    runTest(test_case)
