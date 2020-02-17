cd999e118d2c9c2fa37d857956896a24c9d20b0a
Alex Maloney <maloney.a12@gmail.com>
2020-02-16 17:10:19 -0800

    [Protoype] SQLAlchemy objects for Entity Event Actor and Entry

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+from sqlalchemy import create_engine, Columnm, Integer, String, ForeignKey, Table 
+from sqlalchemy.orm import relationship 
+from sqlalchemy.ext.declarative import declarative_base
 from SentenceParser import EntityEventActorParser
 
 app = Flask(__name__)
+engine = create_engine('sqlite:///:memory', echo=True)
 
 @app.route("/sentence", methods=['POST'])
 def parseSentence():
@@ -10,3 +14,52 @@ def parseSentence():
 
     return " Event: " + parser.event + " Entity: " + parser.entity
     
+
+class Entity(Base):
+    __tablename__ = 'entity'
+
+    id = Column(Integer, primary_key=true)
+    description = Column(String)
+    events = relationship("Event", back_populates="entity")
+
+    def __repr__(self):
+        return "<Entity(description = %s)" % (self.description)
+
+event_actor_association_table = Table('association', Base.metadata,
+    Column('event_id', Integer, ForeignKey("event.id")),
+    Columnm('actor_id', Integer, ForeignKey("actor.id"))
+)
+
+class Event(Base):
+    __tablename__ = 'event'
+
+    id = Column(Integer, primary_key=true)
+    description = Column(String)
+    entry = relationship("Entry", uselist=False. back_populates="event")
+    entity_id = Column(Integer, ForeignKey("entity.id"))
+    entitiy = relationship("Entity", back_populates="events")
+    actors = relationship('Actor', secondary=event_actor_association_table, back_populates='events')
+
+    def __repr__(self):
+        return "<Event(description = %s)" % (self.description)
+
+class Actor(Base):
+    __tablename__ = 'actor'
+
+    id = Column(Integer, primary_key=true)
+    name = Column(String)
+    events = relationship('Event', secondary_association=event_actor_association_table, back_populates='actors')
+
+    def __repr__(self):
+        return "<Actor(name = %s)" % (self.name)
+
+class Entry(Base):
+    __tablename__ = 'entry'
+
+    id = Column(Integer, primary_key=true)
+    original_text = Column(String)
+    event_id = Column(Integer, ForeignKey('event.id'))
+    event = relationship('Event', back_populates='entry')
+
+    def __repr__(self):
+        return "<Entry(original_text = %s)" % (self.original_text)
--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo python3 -m pip install -U Flask
 sudo python3 -m pip install -U spacy 
+sudo python3 -m pip install -u sqlalchemy
 python3 -m spacy download en_core_web_sm 
